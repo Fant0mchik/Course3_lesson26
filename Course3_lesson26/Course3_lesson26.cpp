eda31e56f2b947136a3e4359f2a709cdab3444c7
Fantomchik <luchynin09@gmail.com>
2024-12-06 19:22:37 +0200

    Added class point

--- Course3_lesson26/Course3_lesson26.cpp
+++ Course3_lesson26/Course3_lesson26.cpp
@@ -1,6 +1,16 @@
 using namespace std;
 #include <iostream>
 
+class point {
+    int x;
+    int y;
+public:
+    point(int x, int y) : x(x), y(y) {}
+    void print() {
+        cout << x << " " << y << endl;
+    }
+};
+
 int main()
 {
     
