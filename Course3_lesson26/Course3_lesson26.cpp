eae172a1d0ee30663cfa620f130e93438fe1189d
Fantomchik <luchynin09@gmail.com>
2024-12-06 19:41:55 +0200

    Deleted vector of points. Added vector of figures

--- Course3_lesson26/Course3_lesson26.cpp
+++ Course3_lesson26/Course3_lesson26.cpp
@@ -14,21 +14,65 @@ public:
 };
 
 class figure {
+public:
     point p1;
     point p2;
     point p3;
     point p4;
-public:
-    virtual void print() = 0;
+    figure(point p1, point p2, point p3, point p4) : p1(p1), p2(p2), p3(p3), p4(p4) {}
+    virtual void print() = 0; 
+    virtual ~figure() {}     
 };
 
+class square : public figure {
+public:
+    square(point p1, point p2, point p3, point p4) : figure(p1, p2, p3, p4) {}
+    void print() override {
+        cout << "Square: " << endl;
+        p1.print();
+        p2.print();
+        p3.print();
+        p4.print();
+    }
+};
 
+class circle : public figure {
+public:
+    circle(point p1, point p2, point p3, point p4) : figure(p1, p2, p3, p4) {}
+    void print() override {
+        cout << "Circle: THIS IS A CIRCLE, POINTS ARE NOT RELEVANT!" << endl;
+    }
+};
+
+class parallelogram : public figure {
+public:
+    parallelogram(point p1, point p2, point p3, point p4) : figure(p1, p2, p3, p4) {}
+    void print() override {
+        cout << "Parallelogram: " << endl;
+        p1.print();
+        p2.print();
+        p3.print();
+        p4.print();
+    }
+};
 
 int main()
 {
-    vector<point> figure1;
-    figure1.push_back({ 2,6 });
-    figure1.push_back({ 5,8 });
-    figure1.push_back({ -1,2 });
-    figure1.push_back({ 2,-6 });
+    vector<figure*> figures;
+
+
+    figures.push_back(new square({ 1, 1 }, { 1, 4 }, { 4, 4 }, { 4, 1 }));
+    figures.push_back(new circle({ 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }));
+    figures.push_back(new parallelogram({ 2, 3 }, { 6, 3 }, { 5, 5 }, { 1, 5 }));
+
+    for (figure* f : figures) {
+        f->print();
+        cout << endl;
+    }
+
+    for (figure* f : figures) {
+        delete f;
+    }
+
+    return 0;
 }
